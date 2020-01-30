0540a847d18fcdcda30aee3b189af51be8b7ff53
João Simões <nogravity00@outlook.com>
2020-01-30 23:32:05 +0000

    Then extensions for !NET 4.0

--- src/TasksLikePromises/TasksLikePromisesExtensions-NET-OTHER.cs
+++ src/TasksLikePromises/TasksLikePromisesExtensions-NET-OTHER.cs
@@ -5,7 +5,85 @@ namespace System.Threading.Tasks
 {
     public static partial class TasksLikePromisesExtensions
     {
+        public static async Task<TResult> Then<T, TResult>(this Task<T> task, Func<T, Task<TResult>> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
 
+            var t = await task.ConfigureAwait(false);
+
+            return await onFulfilled(t).ConfigureAwait(false);
+        }
+
+        public static async Task<TResult> Then<T, TResult>(this Task<T> task, Func<T, TResult> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            var t = await task.ConfigureAwait(false);
+
+            return onFulfilled(t);
+        }
+
+        public static async Task Then<T>(this Task<T> task, Func<T, Task> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            var t = await task.ConfigureAwait(false);
+
+            await onFulfilled(t).ConfigureAwait(false);
+        }
+
+        public static async Task Then<T>(this Task<T> task, Action<T> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            var t = await task.ConfigureAwait(false);
+
+            onFulfilled(t);
+        }
+
+        public static async Task<TResult> Then<TResult>(this Task task, Func<Task<TResult>> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            await task.ConfigureAwait(false);
+
+            return await onFulfilled().ConfigureAwait(false);
+        }
+
+        public static async Task<TResult> Then<TResult>(this Task task, Func<TResult> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            await task.ConfigureAwait(false);
+
+            return onFulfilled();
+        }
+
+        public static async Task Then(this Task task, Func<Task> onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            await task.ConfigureAwait(false);
+
+            await onFulfilled().ConfigureAwait(false);
+        }
+
+        public static async Task Then(this Task task, Action onFulfilled)
+        {
+            task.NotNull(nameof(task));
+            onFulfilled.NotNull(nameof(onFulfilled));
+
+            await task.ConfigureAwait(false);
+
+            onFulfilled();
+        }
     }
 }
 
