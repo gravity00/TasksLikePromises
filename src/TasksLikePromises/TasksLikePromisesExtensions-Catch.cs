76dce3c6d81143f0b25abefbe3852768d64ba031
João Simões <nogravity00@outlook.com>
2020-02-03 12:13:53 +0000

    Catch extensions for !NET 4.0

---
+++ src/TasksLikePromises/TasksLikePromisesExtensions-Catch.cs
@@ -0,0 +1,78 @@
+﻿#if !NET40
+
+// ReSharper disable once CheckNamespace
+namespace System.Threading.Tasks
+{
+    public static partial class TasksLikePromisesExtensions
+    {
+        public static async Task<T> Catch<T>(this Task<T> task, Func<Exception, Task<T>> onRejected)
+        {
+            task.NotNull(nameof(task));
+            onRejected.NotNull(nameof(onRejected));
+
+            try
+            {
+                return await task.ConfigureAwait(false);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                return await onRejected(e).ConfigureAwait(false);
+            }
+        }
+
+        public static async Task<T> Catch<T>(this Task<T> task, Func<Exception, T> onRejected)
+        {
+            task.NotNull(nameof(task));
+            onRejected.NotNull(nameof(onRejected));
+
+            try
+            {
+                return await task.ConfigureAwait(false);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                return onRejected(e);
+            }
+        }
+
+        public static async Task Catch(this Task task, Func<Exception, Task> onRejected)
+        {
+            task.NotNull(nameof(task));
+            onRejected.NotNull(nameof(onRejected));
+
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                await onRejected(e).ConfigureAwait(false);
+            }
+        }
+
+        public static async Task Catch(this Task task, Action<Exception> onRejected)
+        {
+            task.NotNull(nameof(task));
+            onRejected.NotNull(nameof(onRejected));
+
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                onRejected(e);
+            }
+        }
+    }
+}
+
+#endif
